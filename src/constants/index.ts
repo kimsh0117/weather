ae80edef2e057995dbc6cb0aa1841e6fd845eeb8
sanghun <crea.id.1703@gmail.com>
2023-08-12 23:24:36 +0300

    delete api key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const WEATHER = 'weather'
 export const WEATHER_API_URL = 'https://api.openweathermap.org'
 
-export const WEATHER_API_KEY = 'd378158b3bbc1910e13e55f70017426c'
+export const WEATHER_API_KEY = ''
 
 export const WEATHER_ICON_URL = 'https://openweathermap.org/img/wn/'
